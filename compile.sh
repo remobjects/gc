a902f1a81cd5f2a72093e6da3fb8503da80959c8
marc hoffman <mh@remobjects.com>
2021-06-02 08:54:20 -0400

    Script fixes

--- compile.sh
+++ compile.sh
@@ -1,80 +1,100 @@
 #!/bin/bash
 
 export COMMONFLAGS=" -Wno-deprecated-declarations -Wno-nullability-completeness -Wno-unknown-attributes -Wno-builtin-requires-header -D HAVE_DL_ITERATE_PHDR -D GC_THREADS -I libatomic_ops\src -Wno-expansion-to-defined -Wno-ignored-attributes -Ilibatomic_ops/src "
-export XCODE="/Users/mg/Applications/Xcode-12-Beta2-with-ARM.app"
+export XCODE="/Users/mh/Applications/Xcode-12.3-Beta1.app"
 
 # macOS
 
+export TARGET=macos
+
 export TRIPLE=x86_64-apple-macosx
-export OSXINC=$XCODE/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/
+export OSXINC=$XCODE/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/
 ./innercompile.sh
 
 export TRIPLE=arm64-apple-macosx
-export OSXINC=$XCODE/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/
+export OSXINC=$XCODE/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/
 ./innercompile.sh
 
 
 # UIKit for Mac
 
+export TARGET=catalyst
+
 export TRIPLE=x86_64-apple-ios-macabi
-export OSXINC=$XCODE/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/
+export OSXINC=$XCODE/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/
 ./innercompile.sh
 
 export TRIPLE=arm64-apple-ios-macabi
-export OSXINC=$XCODE/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/
+export OSXINC=$XCODE/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/
 ./innercompile.sh
 
 
 # iOS
 
+export TARGET=ios
 export TRIPLE=arm64-apple-ios
-export OSXINC=$XCODE/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/include
+export OSXINC=$XCODE/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
 ./innercompile.sh
 
 export TRIPLE=armv7s-apple-ios
-export OSXINC=$XCODE/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/include
+export OSXINC=$XCODE/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
 ./innercompile.sh
 
 export TRIPLE=armv7-apple-ios
-export OSXINC=$XCODE/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/include
+export OSXINC=$XCODE/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
+./innercompile.sh
+
+export TARGET=iossimulator
+export TRIPLE=arm64-apple-ios
+export OSXINC=$XCODE/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk
 ./innercompile.sh
 
 export TRIPLE=x86_64-apple-ios
-export OSXINC=$XCODE/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/include
+export OSXINC=$XCODE/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk
 ./innercompile.sh
 
 export TRIPLE=i386-apple-ios
-export OSXINC=$XCODE/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/include
+export OSXINC=$XCODE/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk
 ./innercompile.sh
-
 
 # tvOS
 
+export TARGET=tvos
 export TRIPLE=arm64-apple-tvos
-export OSXINC=$XCODE/Contents/Developer/Platforms/AppleTVOS.platform/Developer/SDKs/AppleTVOS.sdk/usr/include
+export OSXINC=$XCODE/Contents/Developer/Platforms/AppleTVOS.platform/Developer/SDKs/AppleTVOS.sdk
+./innercompile.sh
+
+export TARGET=tvos=simulator
+export TRIPLE=arm64-apple-tvos
+export OSXINC=$XCODE/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk
 ./innercompile.sh
 
 export TRIPLE=x86_64-apple-tvos
-export OSXINC=$XCODE/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk/usr/include
+export OSXINC=$XCODE/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk
 ./innercompile.sh
-
 
 # watchOS
 
+export TARGET=watchos
 export TRIPLE=arm64_32-apple-watchos
-export OSXINC=$XCODE/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk/usr/include
+export OSXINC=$XCODE/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk
 #./innercompile.sh
 
 export TRIPLE=armv7k-apple-watchos
-export OSXINC=$XCODE/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk/usr/include
+export OSXINC=$XCODE/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk
+#./innercompile.sh
+
+export TARGET=iosimulator
+export TRIPLE=arm64-apple-watchos
+export OSXINC=$XCODE/Contents/Developer/Platforms/Watchimulator.platform/Developer/SDKs/WatchSimulator.sdk
 #./innercompile.sh
 
 export TRIPLE=x86_64-apple-watchos
-export OSXINC=$XCODE/Contents/Developer/Platforms/Watchimulator.platform/Developer/SDKs/WatchSimulator.sdk/usr/include
+export OSXINC=$XCODE/Contents/Developer/Platforms/Watchimulator.platform/Developer/SDKs/WatchSimulator.sdk
 #./innercompile.sh
 
 export TRIPLE=i386-apple-watchos
-export OSXINC=$XCODE/Contents/Developer/Platforms/Watchimulator.platform/Developer/SDKs/WatchSimulator.sdk/usr/include
+export OSXINC=$XCODE/Contents/Developer/Platforms/Watchimulator.platform/Developer/SDKs/WatchSimulator.sdk
 #./innercompile.sh
 
 
--- innercompile.sh
+++ innercompile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-export COMPILEFLAGS="-O3 -c -I $OSXINC -I ../libatomic_ops/src -I include -Wno-deprecated-declarations -Wno-unknown-attributes -Wno-builtin-requires-header -fPIC $COMMONFLAGS  -D_REENTRANT"
+export COMPILEFLAGS="-O3 -c -isysroot $OSXINC -I ../libatomic_ops/src -I include -Wno-deprecated-declarations -Wno-unknown-attributes -Wno-builtin-requires-header -fPIC $COMMONFLAGS  -D_REENTRANT"
 rm *.o 
 echo "Triple: $TRIPLE Release"
 clang $COMPILEFLAGS --target=$TRIPLE alloc.c reclaim.c allchblk.c misc.c mach_dep.c os_dep.c mark_rts.c headers.c mark.c obj_map.c blacklst.c finalize.c new_hblk.c dbg_mlc.c fnlz_mlc.c malloc.c  dyn_load.c typd_mlc.c ptr_chck.c  mallocx.c pthread_support.c darwin_stop_world.c pthread_start.c
-ar rcs gc-$TRIPLE.a alloc.o reclaim.o allchblk.o misc.o mach_dep.o os_dep.o mark_rts.o headers.o mark.o obj_map.o blacklst.o finalize.o new_hblk.o dbg_mlc.o fnlz_mlc.o malloc.o  dyn_load.o typd_mlc.o ptr_chck.o  mallocx.o pthread_support.o darwin_stop_world.o pthread_start.o
+ar rcs gc-$TARGET-$TRIPLE.a alloc.o reclaim.o allchblk.o misc.o mach_dep.o os_dep.o mark_rts.o headers.o mark.o obj_map.o blacklst.o finalize.o new_hblk.o dbg_mlc.o fnlz_mlc.o malloc.o  dyn_load.o typd_mlc.o ptr_chck.o  mallocx.o pthread_support.o darwin_stop_world.o pthread_start.o
